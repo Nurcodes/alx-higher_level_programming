7fda0817c0f3fd97d914950f6f3bdb3edf1264a9
Abdifatah Nur <deenynur21@gmail.com>
2022-06-01 02:28:32 +0300

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for i in range(0, len(str)):
         if str[i] == ' ':
             print(" ", end='')
-        elif str[i] > 64 and str[i] < 91:
+        elif ord(str[i]) > 64 and ord(str[i]) < 91:
             continue
         else:
             print("{:c}".format(ord(str[i]) - 32), end='')
