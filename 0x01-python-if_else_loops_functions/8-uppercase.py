5fe1b6663dc538e0554b1b6f9981989cb8ddf45c
Abdifatah Nur <deenynur21@gmail.com>
2022-06-01 03:48:55 +0300

    Task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,4 +5,4 @@ def uppercase(str):
         if uni_code > 96 and uni_code < 123:
             uni_code = uni_code - 32
         print("{}".format(chr(uni_code)), end='')
-    print('\n')
+    print()
