3711acd48677e9c5cc6932645ba1ab97f881bbb9
Abdifatah Nur <deenynur21@gmail.com>
2022-06-03 00:10:38 +0300

    Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    l_en = len(sys.argv)
+    b = 0
+    s_um = 0
+    if (l_en == 1):
+        print("0")
+    elif (l_en > 1):
+        b = 1
+        while (b < l_en):
+            s_um += int(sys.argv[b])
+            b += 1
+        print(s)
