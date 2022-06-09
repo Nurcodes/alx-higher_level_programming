05038f91fa619e6ad907a7d8ceed100f26e05321
Abdifatah Nur <deenynur21@gmail.com>
2022-06-09 08:37:35 +0300

    Task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is None:
-        return "None"
+        return(None)
     else:
         for i, j in sorted(a_dictionary.items()):
             high = i
-        return high
+        return(high)
