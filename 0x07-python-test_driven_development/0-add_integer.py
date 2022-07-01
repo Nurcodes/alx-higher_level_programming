fd67d3364f79973d2593b45e85e7232758e3fdd8
Abdifatah Nur <deenynur21@gmail.com>
2022-07-02 00:15:48 +0300

    Task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" This function takes two args: a and b
+    converts them to ints from floats if they are
+    are computes the sum.
+    Return Value will be a + b
+    raise exceptions for certain edge cases detailed"""
+
+def add_integer(a, b=98):
+    '''Returns an int: the sum of a and b
+    Args: a (int): first arg
+          b (int): second arg'''
+
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
+
