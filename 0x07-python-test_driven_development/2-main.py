acf3f26fa9327a8340b299538ece498ab9d4b0ff
Abdifatah Nur <deenynur21@gmail.com>
2022-07-02 05:02:59 +0300

    Task 0 and 1

---
+++ 0x07-python-test_driven_development/2-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [
+    'f',
+    [2,2]
+]
+print(matrix_divided(matrix, 1))
+print(matrix)
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""This module defines a function
+    which divides all elements of of a matrix
+    by a given int"""
+
+
+def matrix_divided(matrix, div):
+    """Divides a 2d matrix
+        by a given number
+        Args: matrix(2dmatrix), div(int)"""
+    flag = False
+    first = "matrix must be a matrix "
+    second = "(list of lists) of integers/floats"
+
+    for i in matrix:
+        for j in i:
+            if not isinstance(j, int) and not isinstance(j, float):
+                flag = True
+                break
+    if flag:
+        raise TypeError(first + second)
+
+    rs = []
+    for row in matrix:
+        rs.append(len(row))
+    first = rs[0]
+    result = True
+    for i in rs:
+        if first != i:
+            result = False
+            break
+        else:
+            continue
+    if not result:
+        raise TypeError("Each row of the matrix must have the same size")
+
+    if type(div) not in [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new = [item[:] for item in matrix]
+    for i in range(len(new)):
+        for j in range(len(new[i])):
+            new[i][j] = round(new[i][j] / div, 2)
+    return new
