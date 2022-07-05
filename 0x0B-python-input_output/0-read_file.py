ea14cf8929d836268f8d12a7962e088fa8931561
Abdifatah Nur <deenynur21@gmail.com>
2022-07-05 23:23:24 +0300

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python
-
 """ This module creates a function which reads a text file """
 
 
 def read_file(filename=""):
     """ reads a file with with statement """
+
     with open(filename, 'r', encoding='utf-8') as f:
+        """ using open and with statements """
         print(f.read(), end='')
