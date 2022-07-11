7287baa88296b3f65f436dd4d83f5de163873d93
Abdifatah Nur <deenynur21@gmail.com>
2022-07-12 01:11:24 +0300

    Task 1

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Test for base.py"""
+import unittest
+from models import Base
+
+class base_t(unittest.Testcase):
+    """ test the id """
+    def test_base_id(self):
+        """ first test method """
+        pass
