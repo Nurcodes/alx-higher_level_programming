dee13500dcf5f57b74b0bcbae254cff05d6cad9a
Abdifatah Nur <deenynur21@gmail.com>
2022-09-07 17:01:53 +0300

    t5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const num = Math.floor(Number(process.argv[2]));
-if (isNaN(num)) {
+if (isNaN(num) || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + num);
