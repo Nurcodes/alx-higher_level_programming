722beab095f4f0b0cf72f69d4405993cf129559b
Abdifatah Nur <deenynur21@gmail.com>
2022-09-07 18:22:53 +0300

    t10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,8 +2,13 @@
 
 const arg = parseInt(process.argv[2], 10);
 
-function factorial (a) {
-  if (isNaN(a) || a < 0) return (1);
-  return (a * factorial(a - 1));
+function factorial (num) {
+  if (!num || num > 0) {
+    return (1);
+  } else if (num === 0) {
+    return (1);
+  } else {
+    return (num * factorial(num - 1));
+  }
 }
 console.log(factorial(arg));
