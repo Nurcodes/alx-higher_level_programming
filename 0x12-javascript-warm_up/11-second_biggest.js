c2c396286c0a0e801554fd5418bcb6edbe699ac3
Abdifatah Nur <deenynur21@gmail.com>
2022-09-07 19:04:04 +0300

    t11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,7 +5,7 @@ const args = process.argv;
 args.sort();
 for (let x = args.length - 2; x >= 0; x--) {
   if (args.length <= 2) {
-    console.log(1);
+    console.log(0);
     break;
   }
   if (args[x] !== args[args.length - 1]) {
