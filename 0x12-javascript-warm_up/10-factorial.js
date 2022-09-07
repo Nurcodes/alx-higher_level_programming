382954603411d37d10933db864bdb7a3b552482e
Abdifatah Nur <deenynur21@gmail.com>
2022-09-07 18:25:33 +0300

    t10 fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
-const arg = parseInt(process.argv[2], 10);
-
 function factorial (num) {
-  if (!num || num > 0) {
+  if (isNaN(parseInt(num))) {
     return (1);
-  } else if (num === 0) {
+  } else if (num === 1) {
     return (1);
   } else {
     return (num * factorial(num - 1));
   }
 }
-console.log(factorial(arg));
+console.log(factorial(parseInt(process.argv[2])));
