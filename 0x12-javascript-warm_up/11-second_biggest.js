538937ce08fff1cceb39c093d1107c28b1c2aa70
Abdifatah Nur <deenynur21@gmail.com>
2022-09-07 23:23:59 +0300

    t11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,18 +3,9 @@
 function second (myArray) {
   if (myArray.length === 2 || myArray.length === 3) { return (0); }
 
-  let max = myArray[2];
-  let secondMax = myArray[3];
-
-  for (let i = 2; i < myArray.length; i++) {
-    if (myArray[i] > max) {
-      secondMax = max;
-      max = myArray[i];
-    } else if (myArray[i] > secondMax && myArray[i] < max) {
-      secondMax = myArray[i];
-    }
-  }
-  return (secondMax);
+  myArray.sort();
+  const res = myArray[myArray.length - 2];
+  return res;
 }
 
 console.log(second(process.argv));
