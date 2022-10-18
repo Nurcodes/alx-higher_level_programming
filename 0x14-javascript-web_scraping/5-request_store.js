52916f9439c381ce9542094349ed404c97793a95
Abdifatah Nur <deenynur21@gmail.com>
2022-10-18 13:10:56 -0700

    finished

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Store the contents of a body response to a file
+
+const fs = require('fs');
+const request = require('request');
+request(process.argv[2].pipe(fs.createWriteStream(process.argv[3])));
